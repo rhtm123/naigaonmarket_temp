10324ded65c2dc7a11ec33ff32f8e19af40650fb
Rohit Maurya <maurya.iitk@gmail.com>
2025-03-25 11:52:18 +0530

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,7 +7,7 @@
   let seconds = 0;
   
   // Set your launch date here
-  const launchDate = new Date('2025-03-24T00:00:00');
+  const launchDate = new Date('2025-04-01T00:00:00');
   
   function updateCountdown() {
     const now = new Date();
